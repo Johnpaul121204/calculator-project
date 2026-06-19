ccd574ba16f51a00e85b92fd04d6060a0dd4e01f
Johnpaul121204 <pauljohn8595@gmail.com>
2026-06-19 07:33:24 +0000

    frontend project learned while practicing frontend development

---
+++ app.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+
+  const handleClick = (value) => {
+    setInput(input + value);
+  };
+
+  const calculate = () => {
+    try {
+      setInput(eval(input).toString());
+    } catch {
+      setInput("Error");
+    }
+  };
+
+  const clearInput = () => {
+    setInput("");
+  };
+
+  const deleteLast = () => {
+    setInput(input.slice(0, -1));
+  };
+
+  return (
+    <div className="container">
+      <div className="calculator">
+        <input type="text" value={input} readOnly className="display" />
+
+        <div className="buttons">
+          <button onClick={clearInput}>C</button>
+          <button onClick={deleteLast}>DEL</button>
+          <button onClick={() => handleClick("%")}>%</button>
+          <button onClick={() => handleClick("/")}>÷</button>
+
+          <button onClick={() => handleClick("7")}>7</button>
+          <button onClick={() => handleClick("8")}>8</button>
+          <button onClick={() => handleClick("9")}>9</button>
+          <button onClick={() => handleClick("*")}>×</button>
+
+          <button onClick={() => handleClick("4")}>4</button>
+          <button onClick={() => handleClick("5")}>5</button>
+          <button onClick={() => handleClick("6")}>6</button>
+          <button onClick={() => handleClick("-")}>-</button>
+
+          <button onClick={() => handleClick("1")}>1</button>
+          <button onClick={() => handleClick("2")}>2</button>
+          <button onClick={() => handleClick("3")}>3</button>
+          <button onClick={() => handleClick("+")}>+</button>
+
+          <button onClick={() => handleClick("0")}>0</button>
+          <button onClick={() => handleClick(".")}>.</button>
+          <button className="equal" onClick={calculate}>
+            =
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
